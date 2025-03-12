ba4aa8b7d30f087df487d3987eee47ae8198bc44
Elena <elep.81@gmail.com>
2025-03-12 16:41:23 +0000

    script js added for getting-started btn

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // TOGGLE MENU
-
 document.addEventListener('DOMContentLoaded', () => {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const navMenu = document.querySelector('nav ul');
@@ -14,4 +13,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // --------------------------------------------------
 
+// GET STARTED MODAL
+document.addEventListener('DOMContentLoaded', function() {
+    const getStartedButton = document.querySelector('.btn-main');
+    const modal = document.getElementById('get-started-modal');
+    const closeModal = document.querySelector('.close-modal');
 
+    if (getStartedButton) {
+        getStartedButton.addEventListener('click', function(event) {
+            event.preventDefault();
+            modal.style.display = 'flex';
+        });
+    }
+
+    if (closeModal) {
+        closeModal.addEventListener('click', function() {
+            modal.style.display = 'none';
+        });
+    }
+
+    // Close modal if clicked outside of it
+    window.addEventListener('click', function(event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    });
+
+});
